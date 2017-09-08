222f75dece4d49255b0b808678fd0f6227f62e47
JIN Benli <benli0822@gmail.com>
2017-09-08 23:16:55 +0800

    Update pseudo code

---
+++ snow.js
@@ -0,0 +1,13 @@
+var myWindowId;
+
+// Need to retrieve inbox id from configuration
+const contentBox = document.querySelector("#context");
+
+// Load pre defined templates
+console.log("test");
+
+// According to users choice, query for template
+
+
+// Setup content box
+contentBox.textContent = 'abc';
